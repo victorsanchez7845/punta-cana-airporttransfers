30a5d72a01facf4ac4261100e6d89822ad8cdf47
victorsanchez7845 <59267510+victorsanchez7845@users.noreply.github.com>
2026-09-24 08:57:23 -0600

    Rename PostResource to PuntaCanaPostResource

--- app/Filament/Resources/PostResource/Pages/ListPosts.php
+++ app/Filament/Resources/PostResource/Pages/ListPosts.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace App\Filament\Resources\PostResource\Pages;
+namespace App\Filament\Resources\PuntaCanaPostResource\Pages;
 
-use App\Filament\Resources\PostResource;
+use App\Filament\Resources\PuntaCanaPostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListPosts extends ListRecords
 {
-    protected static string $resource = PostResource::class;
+    protected static string $resource = PuntaCanaPostResource::class;
 
     protected function getHeaderActions(): array
     {
