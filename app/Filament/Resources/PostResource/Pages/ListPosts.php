8292392122294f833b754852a54740cd456dbfa2
victorsanchez7845 <59267510+victorsanchez7845@users.noreply.github.com>
2026-09-24 09:19:16 -0600

    Refactor ListPosts to use PostResource

--- app/Filament/Resources/PostResource/Pages/ListPosts.php
+++ app/Filament/Resources/PostResource/Pages/ListPosts.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace App\Filament\Resources\PuntaCanaPostResource\Pages;
+namespace App\Filament\Resources\PostResource\Pages;
 
-use App\Filament\Resources\PuntaCanaPostResource;
+use App\Filament\Resources\PostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListPosts extends ListRecords
 {
-    protected static string $resource = PuntaCanaPostResource::class;
+    protected static string $resource = PostResource::class;
 
     protected function getHeaderActions(): array
     {
